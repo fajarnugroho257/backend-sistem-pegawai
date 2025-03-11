977f3afde7da00e6c51302c9f8ead81640d2dbbd
fajarnugroho257 <fajarnugroho274@gmail.com>
2025-03-11 16:19:03 +0700

    update DB config

--- config/db.js
+++ config/db.js
@@ -1,10 +1,22 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("sistem_layanan", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-  logging: false, // Mengurangi log query di terminal
-});
+// const sequelize = new Sequelize("sistem_layanan", "root", "", {
+//   host: "localhost",
+//   dialect: "mysql",
+//   logging: false, // Mengurangi log query di terminal
+// });
+
+const sequelize = new Sequelize(
+  "railway",
+  "root",
+  "wcIERygxhCkJFsWEzxxVPNlDWHnTZqNn",
+  {
+    host: "mainline.proxy.rlwy.net",
+    port: 42371, // Tambahkan port di sini
+    dialect: "mysql",
+    logging: false, // Mengurangi log query di terminal
+  }
+);
 
 const connectDB = async () => {
   try {
